6c2c0eec84d90c886ca789a8f231199c37a7d8fd
marvinSpring <tietian.ma@wimisoft.com>
2022-04-19 18:55:38 +0800

    初始化

---
+++ src/main/java/com/example/demo2/PropertyFactory.java
@@ -0,0 +1,47 @@
+package com.example.demo2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PropertyFactory {
+
+    private static String doReplaceJavaObjectPropertiesToDataBaseField(String property, int length) {
+        if (length > 0) {
+            String upper = null;
+            Character c = null;
+            if (Character.isUpperCase(property.charAt(length - 1))) {
+                c = property.charAt(length - 1);
+                upper = "_" + String.valueOf(c).toLowerCase();
+            }
+            property = doReplaceJavaObjectPropertiesToDataBaseField(upper == null ?property:property.replace(String.valueOf(c), upper), length - 1);
+        }
+        return property;
+    }
+
+    public static String replaceJavaObjectPropertiesToDataBaseField(String property) {
+        if (null==property||property.length()==0){
+            return property;
+        }
+        return doReplaceJavaObjectPropertiesToDataBaseField(property, property.length());
+    }
+
+    public static String replaceDataBaseFieldToJavaObjectProperties(String filed) {
+        StringBuilder filedSb = new StringBuilder(filed);
+        StringBuilder filedSbTemp = new StringBuilder();
+        for (int i = 0; i < filedSb.length(); i++) {
+            if (filedSb.charAt(i)=='_'){
+                char c = filedSb.charAt(i);
+                filedSb = new StringBuilder(filedSb.toString().replace(String.valueOf(c), "\n"));
+
+            }
+        }
+        List<String> filedList = filedSb.toString().lines().skip(1L).collect(Collectors.toList());
+        String first = filedSb.toString().lines().findFirst().get();
+        for (String one : filedList) {
+            char toUpper = one.charAt(0);
+            filedSbTemp.append(one.replaceFirst(String.valueOf(toUpper), String.valueOf(Character.toUpperCase(toUpper))));
+        }
+        return first+filedSbTemp;
+    }
+
+}
---
+++ src/main/java/com/example/demo2/SdkIcons.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class SdkIcons {
+
+  public static final Icon BEAN_TO_DB = IconLoader.getIcon("/icons/BeanToDB.svg", SdkIcons.class);
+
+  public static final Icon DB_TO_BEAN = IconLoader.getIcon("/icons/DBToBean.svg", SdkIcons.class);
+
+}
